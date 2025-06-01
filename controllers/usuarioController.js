c5135d0a1cf043e43a00926180ec5d4634af4140
Vargosky <sarem.vargas@gmail.com>
2025-06-01 17:24:35 -0400

    url directamente en el doc

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -31,7 +31,7 @@ const registrarUsuario = async (req, res) => {
       { expiresIn: '1d' }
     );
 
-    const urlVerificacion = `${process.env.BASE_URL}/api/usuarios/verificar/${token}`;
+    const urlVerificacion = `https://api-para-sistema-git-estructura-modular-vargoskys-projects.vercel.app/api/usuarios/verificar/${token}`;
 
     // ✉️ Configurar transporte de correo
     const transporter = nodemailer.createTransport({
